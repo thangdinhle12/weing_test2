2b1fcd50ee4a7eb58fa089ed69dbcb0efa8ee8a0
thang.le <thangdinhle12@gmail.com>
2024-03-20 15:13:17 +0700

    fix code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './dist/main.css';
+import './scss/menu.scss';
 import reportWebVitals from './reportWebVitals';
 
 
